8f17ee3358d1245cd86eb1dd7382176dcacf1518
Isaac Avram <avrisaac555@gmail.com>
2019-01-06 13:23:05 -0800

    Added exceptions to command.py

--- src/commands/command.py
+++ src/commands/command.py
@@ -1,5 +1,31 @@
 command_map = {}
 
+class CommandError(Exception):
+    """
+    Raised when a command encouters an error that can be resolved by the user. This is also the base
+    class for exceptions in this package.
+    """
+    pass
+
+
+class CommandParseError(CommandError):
+    """
+    Raised when a command encouters and error parsing user-provide input.
+
+    Attributes:
+        name -- Name of the command that caused the error
+        expression -- The expression that caused the error
+        message -- Explanation of the error
+    """
+
+    def __init__(self, name, expression, message):
+        self.name = name
+        self.expression = expression
+        self.message = message
+    
+    def __str__(self):
+        return f"{self.name}: parse error on {self.expression}: {self.message}"
+
 def command(name: str):
     """
     A decorator the defines a command. 
--- src/tests/test_find_AST.py
+++ src/tests/test_find_AST.py
@@ -1,2 +1,5 @@
 from commands.find import ASTPrimary, ASTExpr, ASTBinNot, ASTBinOr, ASTBinAnd
 import pytest
+
+class TestASTPrimary(object):
+    def test_empty_tokens_throws_error
